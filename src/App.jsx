58dee699b184cdf2a8cb56d8493099dbc7bcaff6
vishnu mora <moravishnu2005@gmail.com>
2024-11-25 21:34:49 +0530

    build star rating feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 
 import Accordian from './components/Accordian'
 import RandomColor from './components/random-color-generator'
+import StarRating from './components/star-rating'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +15,10 @@ function App() {
      {/* <Accordian /> */}
 
      {/* Generating random color feature */}
-     <RandomColor />
+     {/* <RandomColor /> */}
+
+     {/* Star rating component */}
+     <StarRating />
     </>
   )
 }
